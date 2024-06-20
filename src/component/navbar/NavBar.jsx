981e45f723e1d7367927451a5d9a2a43c11ec90b
Muhammad Abrar Hassan <muhammadmabrar687@gmail.com>
2024-06-19 20:40:55 -0700

    edit main website name

--- src/component/navbar/NavBar.jsx
+++ src/component/navbar/NavBar.jsx
@@ -11,7 +11,7 @@ const NavBar = () => {
       
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid ">
-    <NavLink className="navbar-brand" to="/home">SKILLS</NavLink> 
+    <NavLink className="navbar-brand" to="/">Business</NavLink> 
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
